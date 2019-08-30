4637ba3e33f9c5566ec003a5b1c895bccdc40851
jmh2012 <jesse.hagewood@gmail.com>
2019-08-29 20:15:41 -0400

    Runbook for updating AIDE db

---
+++ update_AIDE_db.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+require "runbook"
+
+runbook = Runbook.book "AIDE Database Update" do
+   description <<-DESC
+This is a runbook that will update the system's AIDE file integrity 
+database if there are changes.
+   DESC
+
+   section "Update AIDE database" do
+     step "Check if AIDE needs to be updated" do
+       note "Running AIDE check on filesystem..."
+       assert %q{aide.wrapper --check | grep "found differences between 
+database and filesystem"}
+       confirm "Have you checked to see if changes logged in 
+/var/log/aide/aide.log are expected?"
+     end
+     step "Build new AIDE database" do
+       command "aide.wrapper --update"
+     end
+     step "Overwrite old AIDE database" do
+       command "mv -f /var/lib/aide/aide.db.new /var/lib/aide.db"
+     end
+     step "Check if the new database matches filesystem" do
+       assert %q{aide.wrapper --check | grep "All files match AIDE 
+database"}
+     end
+   end
+end
+
+if __FILE__ == $0
+   Runbook::Runner.new(runbook).run
+else
+   runbook
+end
