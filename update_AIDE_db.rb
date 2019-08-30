4d3eb6a939edc88429252eeb5f00233c0f80bf91
jmh2012 <jesse.hagewood@gmail.com>
2019-08-29 22:32:34 -0400

    tmux layout, final fixes

--- update_AIDE_db.rb
+++ update_AIDE_db.rb
@@ -6,21 +6,24 @@ runbook = Runbook.book "AIDE Database Update" do
 This is a runbook that will update the system's AIDE file integrity 
 database if there are changes.
    DESC
-
+  layout [[
+    [:runbook, :log],
+  ]]
    section "Update AIDE database" do
      step "Check if AIDE needs to be updated" do
-       note "Running AIDE check on filesystem..."
-       assert %q{aide --check | grep "found differences between database and filesystem"}
-       confirm "Have you checked to see if changes logged in /var/log/aide/aide.log are expected?"
+       tmux_command "head -n 10 /var/log/aide/aide.log", :log
+       confirm "Have you checked to see if the changes logged in /var/log/aide/aide.log are expected?"
      end
      step "Build new AIDE database" do
-       command "aide --update"
+       command "aide --update | true"
      end
      step "Overwrite old AIDE database" do
-       command "mv -f /var/lib/aide/aide.db.new /var/lib/aide.db"
+       command "mv -f /var/lib/aide/aide.db.new.gz /var/lib/aide/aide.db.gz"
      end
      step "Check if the new database matches filesystem" do
-       assert %q{aide --check | grep "All files match AIDE database"}
+       command "aide --check | true"
+       tmux_command "head -n 10 /var/log/aide/aide.log", :log
+       confirm "Does the new DB match the filesystem?"
      end
    end
 end
@@ -30,3 +33,4 @@ if __FILE__ == $0
 else
    runbook
 end
+
