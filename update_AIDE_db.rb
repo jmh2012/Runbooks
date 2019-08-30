aa2a115fc45db8c42a34c5ee47f821c9e2464c3e
jmh2012 <jesse.hagewood@gmail.com>
2019-08-29 20:18:42 -0400

    replace aide.wrapper with aide command for compatibility

--- update_AIDE_db.rb
+++ update_AIDE_db.rb
@@ -10,19 +10,19 @@ database if there are changes.
    section "Update AIDE database" do
      step "Check if AIDE needs to be updated" do
        note "Running AIDE check on filesystem..."
-       assert %q{aide.wrapper --check | grep "found differences between 
+       assert %q{aide --check | grep "found differences between 
 database and filesystem"}
        confirm "Have you checked to see if changes logged in 
 /var/log/aide/aide.log are expected?"
      end
      step "Build new AIDE database" do
-       command "aide.wrapper --update"
+       command "aide --update"
      end
      step "Overwrite old AIDE database" do
        command "mv -f /var/lib/aide/aide.db.new /var/lib/aide.db"
      end
      step "Check if the new database matches filesystem" do
-       assert %q{aide.wrapper --check | grep "All files match AIDE 
+       assert %q{aide --check | grep "All files match AIDE 
 database"}
      end
    end
